17e5706787061a3b9d929e9c6b9d8bd773e3903c
aasif-create <aasifmmd12345@gmail.com>
2025-09-05 21:34:03 +0530

    ADDED 7TH PATTERN

--- ADVANCED_PATTERNS.java
+++ ADVANCED_PATTERNS.java
@@ -142,5 +142,20 @@ public static void main(String[] args) {
             System.out.println();
         }
         System.out.println("<------------------------------------------------>");
-    }
-}
+        System.out.println("7.HOLLOW RHOMBUS");
+            int n6 = 5;
+            for (int i = 1; i <= n6; i++) {
+                for (int j = 1; j <= n6 - i; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = 1; j <= n6; j++) {
+                    if (i == 1 || i == n1 || j == 1 || j == n6) {
+                        System.out.print("*" + " ");
+                    } else {
+                        System.out.print("  ");
+                    }
+                }
+                System.out.println();
+            }
+
+        }}
