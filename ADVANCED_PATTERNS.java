74d8221941a51d825ab2d1fa13f917346cbbb531
aasif-create <aasifmmd12345@gmail.com>
2025-09-05 23:47:34 +0530

    ADDED 8TH PATTERN

--- ADVANCED_PATTERNS.java
+++ ADVANCED_PATTERNS.java
@@ -161,20 +161,16 @@ public static void main(String[] args) {
             System.out.println("8.PASCAL'S TRIANGLE");
             int n7=5;
             int num=1;
-             for(int i=1 ;i<=n7;i++)
-             {
-                 for(int j=1;j<=n7-i;j++)
-                 {
-                     System.out.print("-");
-                 }
-                 for(int j=1;j<=i;j++) {
+            for(int i=1 ;i<=n7;i++)
+            {
+                for(int j=1;j<=n7-i;j++)
+                {
+                    System.out.print("-");
+                    
+                        System.out.print(j+" ");
+                }
+                }
+            }
 
-                     {
-                         System.out.print(j+" ");
-                     }
-                 }
-                 System.out.println();
-             }
-
-        }}
+        }
         
