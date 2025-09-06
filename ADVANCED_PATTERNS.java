83a2f542124a2f659d489c993dbb06c22d487fe3
aasif-create <aasifmmd12345@gmail.com>
2025-09-06 14:44:51 +0530

    ADDED 8TH PATTERN

--- ADVANCED_PATTERNS.java
+++ ADVANCED_PATTERNS.java
@@ -178,6 +178,7 @@ public static void main(String[] args) {
                 }
                     System.out.println();
             }
+            System.out.println("<------------------------------------------------>");
             
             }
 
