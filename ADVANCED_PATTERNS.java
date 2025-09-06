108a0943f1768b8af312626e2a1c731eb8dbe966
aasif-create <aasifmmd12345@gmail.com>
2025-09-06 15:48:35 +0530

    UPDATED THE FINAL PATTERNS

--- ADVANCED_PATTERNS.java
+++ ADVANCED_PATTERNS.java
@@ -200,11 +200,10 @@ public static void main(String[] args) {
             {
                 for(int j=1 ;j<=n9-i;j++)
                 {
-                    System.out.print(i);
+                    System.out.print(i+" ");
                 }
                 System.out.println();
             }
             }
-
         }
         
