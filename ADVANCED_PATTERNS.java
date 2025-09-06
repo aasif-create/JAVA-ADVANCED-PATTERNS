30cc0738f244bd6ecdcf281c82fb2bb26e587331
aasif-create <aasifmmd12345@gmail.com>
2025-09-06 14:53:17 +0530

    ADDED 8TH PATTERN

--- ADVANCED_PATTERNS.java
+++ ADVANCED_PATTERNS.java
@@ -179,7 +179,20 @@ public static void main(String[] args) {
                     System.out.println();
             }
             System.out.println("<------------------------------------------------>");
-            
+            System.out.println("9.HALF PYRAMID");
+            int n8=5;
+            for(int i=1 ;i<=n8 ;i++)
+            {
+                for(int j=1 ;j<=n8-i;j++)
+                {
+                    System.out.print(" ");
+                }
+                for(int j=1 ; j<=i+1;j++)
+                {
+                    System.out.print(j+" ");
+                }
+                System.out.println();
+            }
             }
 
         }
